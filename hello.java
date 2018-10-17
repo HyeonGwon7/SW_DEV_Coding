d60ee1c59a9a3977b5de15d369ced7a8fb5a5d44
songyungsu <44152392+songyungsu@users.noreply.github.com>
2018-10-17 13:03:46 +0900

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class hello{
   public static void main(String args[]){
     system.out.println("hello Java");
-    system.out.println("chchchchch")
+    system.out.println("chchchchch");
+    system.out.println("Son");
   }
 }
