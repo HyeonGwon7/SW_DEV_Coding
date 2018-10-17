ac64dbdee4a09e690a2cd624291229721c411431
Hyun-Kwon <31947540+Hyun-Kwon@users.noreply.github.com>
2018-10-17 13:54:51 +0900

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,10 +1,9 @@
 class hello{
   public static void main(String args[]){
-    system.out.println("hello Java");
-    system.out.println("chchchchch");
-    system.out.println("Son");
-    systme.out.println("hihi");
-    system.out.println("Yeahhhhhhh");
-
+    System.out.println("hello Java");
+    System.out.println("chchchchch");
+    System.out.println("Son");
+    Systme.out.println("hihi");
+    System.out.println("Yeahhhhhhh");
   }
 }
