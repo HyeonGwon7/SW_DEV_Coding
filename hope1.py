1c69b7d9e7d254bd82785a348a218b30af4490fc
changhwan1 <44152366+changhwan1@users.noreply.github.com>
2018-11-25 23:55:03 +0900

    Add files via upload

--- hope1.py
+++ hope1.py
@@ -44,10 +44,10 @@ def region_of_interest(image, vertices, color3=(255,255,25), color1 = 255):
         ROI_image = cv2.bitwise_and(image,mask)
         return ROI_image
 
-def draw_lines(image, lines, color=[255,0,0], thickness=2):
-        for line in lines:
-        	for x1,y1,x2,y2 in line:
-        		cv2.line(image, (x1,y1),(x2,y2),color,thickness)
+#def draw_lines(image, lines, color=[255,0,0], thickness=2):
+#        for line in lines:
+#        	for x1,y1,x2,y2 in line:
+#        		cv2.line(image, (x1,y1),(x2,y2),color,thickness)
 
 def draw_fit_line(image, lines, color=[255,0,0], thickness=10):
         cv2.line(image,(lines[0],lines[1]), (lines[2],lines[3]), color, thickness)
@@ -61,7 +61,13 @@ def weighted_img(image,initial_img,a=1,b=1.,r=0.):
 
 def get_fitline(image,f_lines):
         lines = np.squeeze(f_lines)
+	print("re")
         lines = lines.reshape(lines.shape[0]*2,2)
+#	print(type(lines))
+#	print(lines)
+#	if lines != lines or lines == lines:
+#		result = []
+#		return result
         rows,cols = image.shape[:2]
         output = cv2.fitLine(lines,cv2.DIST_L2,0,0.01,0.01)
         vx,vy,x,y = output[0],output[1],output[2],output[3]
@@ -101,34 +107,65 @@ for frame in camera.capture_continuous(rawCapture, format = 'bgr',use_video_port
 	print("canny")
 	print(canny_image)
 
-	vertices = np.array([[(50,height),(width/2-45,height/2+60),(width/2+45,height/2+60),(width-50,height)]],dtype=np.int32)
-	print("vertices")
-	print(vertices)
+	ROI = canny_image[160:,:]
+	print("ROI")
+	print(ROI)	
+	
 
-	ROI_image = region_of_interest(canny_image, vertices)
-	print(ROI_image)
-	line_arr = hough_lines(ROI_image, 1, 1*np.pi/180,30,10,20)
+
+
+#	vertices = np.array([[(50,height),(width/2-45,height/2+60),(width/2+45,height/2+60),(width-50,height)]],dtype=np.int32)
+#	print("vertices")
+#	print(vertices)
+#	ROI_image = region_of_interest(canny_image, vertices)
+#	print("ROI_image")
+#	print(ROI_image)
+#ROI_image
+#	line_arr = hough_lines(ROI, 1, 1*np.pi/180,30,10,20)
+	line_arr = hough_lines(ROI, 1, 1*np.pi/180,10,70,10)
+	print("line_arr")
 	print(line_arr)
+#        if line_arr is not None:
+#                a,b,c = line_arr.shape
+#                for i in range(a):
+#                        cv2.line(ROI, (line_arr[i][0][0], line_arr[i][0][1]), (line_arr[i][0][2],line_arr[i][0][3]),(255,0,0),2,cv2.LINE_AA)
+#        else:
+#                print("non")
+#                pass
+
+
+
+	
 	line_arr = np.squeeze(line_arr)
+	print("line_arr sq")
+	print(line_arr)
 	slope_degree = (np.arctan2(line_arr[:, 1]-line_arr[:, 3],line_arr[:, 0]-line_arr[:, 2])*180)/np.pi
+	#slope_degree = (np.arctan2(line_arr[:]-line_arr[:],line_arr[:]-line_arr[:])*180)/np.pi
 
 	line_arr = line_arr[np.abs(slope_degree)<160]
 	slope_degree = slope_degree[np.abs(slope_degree)<160]
 
-	line_arr = line_arr[np.abs(slope_degree)>95]
-	slope_degree = slop_degree[np.abs(slope_degree)>95]
+	line_arr = line_arr[np.abs(slope_degree)>75]
+	slope_degree = slope_degree[np.abs(slope_degree)>75]
 
-	L_lines, R_lines = line_arr[(slope_degree>0),:], line_arr[(slope_degree<0):]
-	temp = np.zeros((image.shape[0], image.shape[1],3), dtype = np.unit8)
+	L_lines, R_lines = line_arr[(slope_degree>0),:], line_arr[(slope_degree<0),:]
+	temp = np.zeros((image.shape[0], image.shape[1],3), dtype = np.uint8)
 	L_lines, R_lines = L_lines[:,None],R_lines[:,None]
-
+	print("lfl")
 	left_fit_line = get_fitline(image,L_lines)
+	print("lfl result")
+	print(left_fit_line)
+	print("rfl")
 	right_fit_line = get_fitline(image,R_lines)
 
 	draw_fit_line(temp, left_fit_line)
-	draw_fitline(temp, right_fit_line)
+	draw_fit_line(temp, right_fit_line)
+	
+#	result = weighted_img(line_arr, image)
+	
 
-	result = weighted_image(temp, image)
-	cv2.imshow('r',result)
-	cv2.waitKey(0)
+	cv2.imshow('r',line_arr)
+#	cv2.imshow('r',ROI)
+#	cv2.imshow('r',line_arr)
+#	cv2.waitKey(0)
 
