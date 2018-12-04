08a814a805e7f9feb74602583f44a3a261b4573c
Hyun-Kwon <31947540+Hyun-Kwon@users.noreply.github.com>
2018-12-05 05:02:50 +0900

    asdasdadssadssdsaaasdadaazgasdgdfhasds fser qfdae

--- Hello.java
+++ Hello.java
@@ -1,20 +1,5 @@
 public class Hello{
 	public static void main(String[] args) {
-		System.out.println("초딩중딩고딩코딩팀은 퇴근하세요~");
-		System.out.println("아~ 잠시만요! 모터는 고치고 가셔야죠!");
-<<<<<<< HEAD
-		System.out.println("아~ 잠시만요! 모터는 고치고 가셔야죠!");
-		System.out.println("아~ 잠시만요! 모터는 고치고 가셔야죠!");
-		System.out.println("아~ 잠시만요! 모터는 고치고 가셔야죠!");
-		System.out.println("아~ 잠시만요! 모터는 고치고 가셔야죠!");
-		System.out.println("아~ 잠시만요! 모터는 고치고 가셔야죠!");
-		System.out.println("아~ 잠시만요! 모터는 고치고 가셔야죠!");
 
-=======sfd
-		System.out.println("이현권");
->>>>>>> branch 'master' of https://github.com/Hyun-Kwon/SW_DEV_Coding.git
-		되다가 안되다가 되다다가 ㄹ안되다다가가
-		
-		System.out.println("되라 제바랄ㄴ아라ㅏㅇ란ㅇㄹ");
 	}
 }
