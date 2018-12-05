0df828a5c17966196f697a4c32a58e089ad5012b
changhwan1 <walkj98@naver.com>
2018-12-05 13:58:01 +0900

    ㅎㅇ

--- Hello.java
+++ Hello.java
@@ -10,8 +10,9 @@ public class Hello{
 		System.out.println("아~ 잠시만요! 모터는 고치고 가셔야죠!");
 		System.out.println("아~ 잠시만요! 모터는 고치고 가셔야죠!");
 
-=======
+=======sfd
 		System.out.println("이현권");
 >>>>>>> branch 'master' of https://github.com/Hyun-Kwon/SW_DEV_Coding.git
+		되다가 안되다가 되다다가 ㄹ안되다다가가
 	}
 }
