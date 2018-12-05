fd0b3826aa87ff0aef6763d0f893322cd1eeaea4
changhwan1 <walkj98@naver.com>
2018-12-05 13:51:40 +0900

    dhk

--- Hello.java
+++ Hello.java
@@ -2,6 +2,12 @@ public class Hello{
 	public static void main(String[] args) {
 		System.out.println("초딩중딩고딩코딩팀은 퇴근하세요~");
 		System.out.println("아~ 잠시만요! 모터는 고치고 가셔야죠!");
-		asd
+		System.out.println("아~ 잠시만요! 모터는 고치고 가셔야죠!");
+		System.out.println("아~ 잠시만요! 모터는 고치고 가셔야죠!");
+		System.out.println("아~ 잠시만요! 모터는 고치고 가셔야죠!");
+		System.out.println("아~ 잠시만요! 모터는 고치고 가셔야죠!");
+		System.out.println("아~ 잠시만요! 모터는 고치고 가셔야죠!");
+		System.out.println("아~ 잠시만요! 모터는 고치고 가셔야죠!");
+
 	}
 }
